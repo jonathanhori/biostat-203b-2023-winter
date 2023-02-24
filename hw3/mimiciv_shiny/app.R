07071f0233b837d6983c4f74e96883148a566910
Jonathan Hori <jonathanhori@Jonathans-MBP-2.lan>
2023-02-24 15:04:39 -0800

    Adding some more structure to shiny app, splitting possible plots into stay/patient variables vs. lab/vital variables.

--- hw3/mimiciv_shiny/app.R
+++ hw3/mimiciv_shiny/app.R
@@ -25,24 +25,83 @@ ui <- fluidPage(
     sidebarPanel(
       helpText("Select a cohort variable to examine."),
       
-      # varSelectInput("var_name", "Variable of interest", cohort),
-      textInput("var_name_s", "Variable of interest", 'first_careunit'),
+      selectInput("var_name", "Variable of interest", 
+                  list(Stay = c(
+                    "First care unit" = "first_careunit",
+                    "Last care unit" = "last_careunit",
+                    "Admission type" = "admission_type",
+                    "Admission location" = "admission_location",
+                    "Discharge location" = "discharge_location"
+                  ),
+                  Patient = c(
+                    "Gender" = "gender",
+                    "Age" = "anchor_age",
+                    "Ethnicity" = "ethnicity",
+                    "Martial status" = "marital_status",
+                    "Language" = "language",
+                    "Insurance" = "insurance"
+                  )
+                  )),
+      # textInput("var_name", "Variable of interest", 'first_careunit'),
       
       
     ),
     
+    
+    
     mainPanel(
-      plotOutput("plot")
+      plotOutput("plot"),
+      tableOutput("summary")
       )
+  ),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Select a lab test or vital measurement to examine."),
+      
+      selectInput("vital_lab_name", "Variable of interest", 
+                  list(Labs = c(
+                    "Bicarbonate" = "bicarbonate",
+                    "Chloride" = "chloride",
+                    "Creatinine" = "creatinine",
+                    "Glucose" = "glucose",
+                    "Potassium" = "potassium",
+                    "Sodium" = "sodium",
+                    "Hemocrit" = "hemocrit",
+                    "White blood cell count" = "white_blood_cells"
+                  ),
+                  Vitals = c(
+                    "Heart rate" = "heart_rate",
+                    "Systolic blood pressure" = "non_invasive_blood_pressure_systolic",
+                    "Average blood pressure" = "non_invasive_blood_pressure_mean",
+                    "Respiratory rate" = "respiratory_rate",
+                    "Body temperature in Fahrenheit" = "temperature_fahrenhe"
+                  )
+                  )),
+      # textInput("var_name", "Variable of interest", 'first_careunit'),
+      
+      
+    ),
+    
+    
+    mainPanel(
+      # plotOutput("plot"),
+      # tableOutput("summary")
+    )
   )
+  
 )
+
+
+
+
 
 # Server logic
 server <- function(input, output) {
   
   dataInput <- reactive({
     cohort %>% 
-      select(input$var_name_s)
+      select(input$var_name)
   })
   
   # output$plot <- renderPlot({
@@ -54,14 +113,16 @@ server <- function(input, output) {
   
   output$plot <- renderPlot({
     dataInput() %>% 
-      ggplot(aes_string(x = input$var_name_s)) +
+      ggplot(aes_string(x = input$var_name)) +
       geom_bar()
     
   })
   
   output$summary <- renderTable({
     dataInput() %>% 
-      group_by(input$var_name_s)
+      # group_by(input$var_name) %>% 
+      summarise(count = n(),
+                average = mean(!!input$var_name, na.rm = TRUE))
   })
   
 }
